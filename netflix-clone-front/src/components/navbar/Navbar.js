3b5cee1d0c1a7b1854ceff87ffb642a7352d585e
piusgori <piusgori@gmail.com>
2022-10-08 12:37:52 +0300

    Added navigation bar

---
+++ netflix-clone-front/src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './navbar.scss';
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
+
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    }
+
+  return (
+    <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
+        <div className='container'>
+            <div className='left'>
+                <img alt='netflix logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png'></img>
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New and Popular</span>
+                <span>My List</span>
+            </div>
+            <div className='right'>
+                <Search className='icon'></Search>
+                <span>KID</span>
+                <Notifications className='icon'></Notifications>
+                <img alt='profile' src='https://cdn.pixabay.com/photo/2017/04/01/21/06/portrait-2194457__340.jpg'></img>
+                <div className='profile'>
+                    <ArrowDropDown className='icon'></ArrowDropDown>
+                    <div className='options'>
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
