fdd51ff33f06a2569da639679f50db35dcd5eaf6
piusgori <piusgori@gmail.com>
2022-10-19 12:39:10 +0300

    added user controllers

--- netflix-clone-back/controllers/user-controller.js
+++ netflix-clone-back/controllers/user-controller.js
@@ -3,6 +3,26 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const HttpError = require('../models/http-error');
 
+exports.getUser = async (req, res, next) => {
+    const userId = req.params.id;
+    try {
+        const foundUser = await User.findById(userId);
+        return res.status(200).json(foundUser);
+    } catch (err) {
+        return next(new HttpError('Error occured'));
+    }
+}
+
+exports.getAll = async (req, res, next) => {
+    const query = req.query.new;
+    try {
+        const users = query ? await User.find().limit(10) : await User.find();
+        return res.status(200).json(users);
+    } catch (err) {
+        return next(new HttpError('Unable to get users'));
+    }
+}
+
 exports.update = async (req, res, next) => {
     const userId = req.params.id;
     const { password } = req.body;
@@ -10,25 +30,25 @@ exports.update = async (req, res, next) => {
         try {
             password = password ? await bcrypt.hash(password, 12) : null;
             const updatedUser = await User.findByIdAndUpdate(userId, { $set: req.body }, { new: true });
-            res.status(200).json(updatedUser);
+            return res.status(200).json(updatedUser);
         } catch (err) {
             return next(new HttpError('Unable to process'));
         }
     } else {
-        return next(new HttpError('Only update your account', 403));
+        return next(new HttpError('Only update your account', null, 403));
     }
 }
 
-exports.delete = async (req, res, next) => {
+exports.delete = async (req, res, next) =>  {
     const userId = req.params.id;
     if (req.user.id === userId || req.user.isAdmin) {
         try {
             await User.findByIdAndDelete(userId)
-            res.status(200).json(updatedUser);
+            return res.status(200).json('User has been deleted');
         } catch (err) {
             return next(new HttpError('Unable to process'));
         }
     } else {
-        return next(new HttpError('Only update your account', 403));
+        return next(new HttpError('Only update your account', null, 403));
     }
 }
--- netflix-clone-back/routes/user-route.js
+++ netflix-clone-back/routes/user-route.js
@@ -5,6 +5,9 @@ const verify = require('../middleware/verify-token');
 
 const router = express.Router();
 
+router.get('/', verify, userController.getAll);
+router.get('/find/:id', userController.getUser);
 router.put('/:id', verify, userController.update);
+router.delete('/:id', verify, userController.delete);
 
 module.exports = router;
